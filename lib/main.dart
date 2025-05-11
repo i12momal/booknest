ee56bd96c0b6bf69d777970a2fd600dfa154e53e
Luz Marina Montilla Marin <i12momal@uco.es>
2025-05-11 19:24:57 +0200

    feat: Main view

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:booknest/views/book_details_owner_view.dart';
-import 'package:booknest/views/home_view.dart';
-import 'package:booknest/views/owner_profile_view.dart';
-import 'package:booknest/views/user_profile_view.dart';
+import 'package:booknest/views/register_view.dart';
 import 'package:flutter/material.dart'; 
 import 'services/base_service.dart';
 import 'views/login_view.dart';
@@ -44,146 +41,122 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomeView()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(screenWidth * 0.8, 50),
-                backgroundColor: const Color(0xFF61BBFF),
-                side: const BorderSide(
-                  color: Colors.white,
-                  width: 2,
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              ),
-              child: const Text(
-                'Home View',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            width: screenWidth * 0.98,
+            height: screenHeight * 0.95,
+            decoration: BoxDecoration(
+              border: Border.all(color: const Color(0xFF112363), width: 4),
+              borderRadius: BorderRadius.circular(20),
             ),
-            const SizedBox(height: 20), // Espacio entre los botones
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginView()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(screenWidth * 0.8, 50),
-                backgroundColor: const Color(0xFF61BBFF),
-                side: const BorderSide(
-                  color: Colors.white,
-                  width: 2,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 35),
+            child: Column(
+              children: [
+                // TÍTULO (parte superior)
+                Image.asset(
+                  'assets/images/titulo.png',
+                  width: double.infinity,
+                  height: screenHeight * 0.15,
+                  fit: BoxFit.contain,
                 ),
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              ),
-              child: const Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+
+                // LOGO (centrado, más grande, hacia el medio)
+                Expanded(
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      width: screenWidth * 0.9,
+                      height: screenHeight * 0.6,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
                 ),
-              ),
+
+                // BOTONES
+                Container(
+                  width: screenWidth * 0.85,
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [Color(0xFF687CFF), Color(0xFF2E3C94)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: const Color(0xFF112363),
+                      width: 3,
+                    ),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 5,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  padding: EdgeInsets.all(screenWidth * 0.05),
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const LoginView()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(screenWidth * 0.8, 50),
+                          backgroundColor: const Color(0xFFAD0000),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            side: const BorderSide(color: Colors.white, width: 3),
+                          ),
+                        ),
+                        child: const Text(
+                          'Iniciar Sesión',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const RegisterView()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(screenWidth * 0.8, 50),
+                          backgroundColor: const Color(0xFFAD0000),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            side: const BorderSide(color: Colors.white, width: 3),
+                          ),
+                        ),
+                        child: const Text(
+                          'Registro',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const OwnerProfileView(userId: 'c3560a09-f261-4f39-ae15-bf6d16ea9b2b')),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(screenWidth * 0.8, 50),
-                backgroundColor: const Color(0xFF61BBFF),
-                side: const BorderSide(
-                  color: Colors.white,
-                  width: 2,
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              ),
-              child: const Text(
-                'Profile owner',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const UserProfileView(userId: 'c3560a09-f261-4f39-ae15-bf6d16ea9b2b')),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(screenWidth * 0.8, 50),
-                backgroundColor: const Color(0xFF61BBFF),
-                side: const BorderSide(
-                  color: Colors.white,
-                  width: 2,
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              ),
-              child: const Text(
-                'Profile user',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const BookDetailsOwnerView(bookId : 1)),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(screenWidth * 0.8, 50),
-                backgroundColor: const Color(0xFF61BBFF),
-                side: const BorderSide(
-                  color: Colors.white,
-                  width: 2,
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              ),
-              child: const Text(
-                'Book Details',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
